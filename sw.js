931a494004ec1a5e67faff06b084ba8eeda048b6
peccur9xe <dmeicler@gmail.com>
2026-07-19 20:49:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'a-nous-v3';
-const ASSETS = ['./','./index.html','./styles.css?v=3','./app.js?v=3','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'a-nous-v4';
+const ASSETS = ['./','./index.html','./styles.css?v=4','./app.js?v=4','./manifest.webmanifest','./icon.svg','./venue-bistro.jpg','./venue-family.jpg','./venue-rooftop.jpg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
